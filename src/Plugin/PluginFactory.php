3fbc66e2bbe78e362d8301522673d2afb7b22094
Austin Passy <367897+thefrosty@users.noreply.github.com>
2019-02-13 11:13:12 -0800

    Revert new property and methods in favor of standalone class.

--- src/Plugin/PluginFactory.php
+++ src/Plugin/PluginFactory.php
@@ -8,8 +8,6 @@ namespace TheFrosty\WpUtilities\Plugin;
  */
 class PluginFactory
 {
-    const WP_ORG_UPDATE_CHECK = 'https://api.wordpress.org/plugins/update-check/';
-
     /**
      * Create a plugin instance.
      *
@@ -17,10 +15,9 @@ class PluginFactory
      * @param string|null $filename Optional. Absolute path to the main plugin file.
      *                         This should be passed if the calling file is not
      *                         the main plugin file.
-     * @param bool $disable_check Disable the plugin from being checked against the WP.org plugin repo?
      * @return Plugin A Plugin object instance.
      */
-    public static function create(string $slug, ?string $filename = '', bool $disable_check = false) : Plugin
+    public static function create(string $slug, ?string $filename = '') : Plugin
     {
         // Use the calling file as the main plugin file.
         if (empty($filename)) {
@@ -38,36 +35,6 @@ class PluginFactory
             ->setUrl(\plugin_dir_url($filename));
 
         $plugin = self::setContainer($plugin);
-        if ($disable_check) {
-            $plugin = self::disablePluginUpdateCheck($plugin);
-        }
-
-        return $plugin;
-    }
-
-    /**
-     * Disable plugin update checks for the current plugin
-     * @param Plugin $plugin
-     * @return Plugin
-     */
-    private static function disablePluginUpdateCheck(Plugin $plugin) : Plugin
-    {
-        /**
-         * Filters the arguments used in an HTTP request.
-         * @link https://stackoverflow.com/a/39217270/558561
-         * @param array $r An array of HTTP request arguments.
-         * @param string $url The request URL.
-         * @return array
-         */
-        \add_filter('http_request_args', function (array $args, string $url) use ($plugin) : array {
-            if (\strpos($url, self::WP_ORG_UPDATE_CHECK) === 0) {
-                $plugins = \json_decode($args['body']['plugins'], true);
-                unset($plugins['plugins'][$plugin->getBasename()]);
-                unset($plugins['active'][\array_search($plugin->getBasename(), $plugins['active'], true)]);
-                $args['body']['plugins'] = \wp_json_encode($plugins);
-            }
-            return $args;
-        });
 
         return $plugin;
     }
