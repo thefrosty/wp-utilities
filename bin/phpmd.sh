3e231c9b39536079d4fcaff58f05f9ea0e1daab0
Austin Passy <367897+thefrosty@users.noreply.github.com>
2019-08-27 10:59:50 -0700

    Include new bin directory with tools for builds and testing in travis.

---
+++ bin/phpmd.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Go to root of the repository
+echo 'Checking PHPMD';
+
+if [ `git rev-parse --verify HEAD` ]; then
+	against='HEAD'
+elif [ `git rev-parse --verify develop` ]; then
+	against='develop'
+else
+    echo "git can't verify HEAD or develop.";
+    exit 1;
+fi
+
+commitFiles=`git diff --name-only $(git merge-base develop ${against})`
+
+args="text phpmd-ruleset.xml --exclude vendor --suffixes php"
+phpFiles="";
+phpFilesCount=0;
+for f in ${commitFiles}; do
+	if [[ ! -e ${f} ]]; then
+		continue;
+	fi
+	if [[ ${f} =~ \.(php|ctp)$ ]]; then
+		phpFilesCount=${phpFilesCount}+1
+		phpFiles="$phpFiles $f"
+	fi
+done;
+if [[ ${phpFilesCount} = 0 ]]; then
+    echo "No PHP files updated, nothing to check.";
+	exit 0;
+fi
+
+echo "Checking files: $phpFiles";
+
+for file in ${phpFiles}; do
+    ./vendor/bin/phpmd ${file} ${args}
+done;
