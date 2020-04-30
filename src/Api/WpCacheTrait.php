84bd5a1f691e43d1cd377770a5b468cf2991b7f5
Austin Passy <367897+thefrosty@users.noreply.github.com>
2020-04-30 14:50:36 -0700

    Update all property references to the method so the correct group is called.

--- src/Api/WpCacheTrait.php
+++ src/Api/WpCacheTrait.php
@@ -52,7 +52,7 @@ trait WpCacheTrait
      */
     protected function getCache(string $key, ?string $group = null, bool $force = false, ?bool &$found = null)
     {
-        return \wp_cache_get($key, $group ?? $this->group, $force, $found);
+        return \wp_cache_get($key, $group ?? $this->getCacheGroup(), $force, $found);
     }
 
     /**
@@ -67,7 +67,7 @@ trait WpCacheTrait
      */
     protected function setCache(string $key, $value, ?string $group = null, int $expiration = 0): bool
     {
-        return \wp_cache_set($key, $value, $group ?? $this->group, $expiration);
+        return \wp_cache_set($key, $value, $group ?? $this->getCacheGroup(), $expiration);
     }
 
     /**
