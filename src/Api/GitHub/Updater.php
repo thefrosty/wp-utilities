84c41b8c98a469de41a9c79297a2601017c3a0eb
Austin Passy <367897+thefrosty@users.noreply.github.com>
2020-09-12 14:44:50 -0700

    Supress PHPMD

--- src/Api/GitHub/Updater.php
+++ src/Api/GitHub/Updater.php
@@ -25,6 +25,7 @@ use TheFrosty\WpUtilities\Plugin\WpHooksInterface;
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * @package TheFrosty\WpUtilities\Api\GitHub
+ * @SuppressWarnings(PHPMD)
  */
 class Updater implements WpHooksInterface
 {
