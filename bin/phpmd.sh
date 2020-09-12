c808a19e2086747b2680236bed2079980256954b
Austin Passy <367897+thefrosty@users.noreply.github.com>
2020-09-12 10:11:19 -0700

    Formatting, ignore vendor dir.

--- bin/phpmd.sh
+++ bin/phpmd.sh
@@ -6,37 +6,37 @@ set -e
 echo 'Checking PHPMD'
 
 if [[ $(git rev-parse --verify HEAD) ]]; then
-	against='HEAD'
+  against='HEAD'
 elif [[ $(git rev-parse --verify develop) ]]; then
-	against='develop'
+  against='develop'
 elif [[ $(git rev-parse --verify master) ]]; then
-	against='master'
+  against='master'
 else
-	echo "git can't verify HEAD, develop or master."
-	exit 1
+  echo "git can't verify HEAD, develop or master."
+  exit 1
 fi
 
 commitFiles=$(git diff --name-only "$(git merge-base develop ${against})")
 
-args="text phpmd-ruleset.xml --exclude vendor --suffixes php"
+args="text phpmd-ruleset.xml --exclude tests,vendor --suffixes php"
 phpFiles=""
 phpFilesCount=0
 for f in ${commitFiles}; do
-	if [[ ! -e ${f} ]]; then
-		continue
-	fi
-	if [[ ${f} =~ \.(php|ctp)$ ]]; then
-		phpFilesCount=$((phpFilesCount + 1))
-		phpFiles="$phpFiles $f"
-	fi
+  if [[ ! -e ${f} ]]; then
+    continue
+  fi
+  if [[ ${f} =~ \.(php|ctp)$ ]]; then
+    phpFilesCount=$((phpFilesCount + 1))
+    phpFiles="$phpFiles $f"
+  fi
 done
 if [[ ${phpFilesCount} == 0 ]]; then
-	echo "No PHP files updated, nothing to check."
-	exit 0
+  echo "No PHP files updated, nothing to check."
+  exit 0
 fi
 
 echo "Checking files: $phpFiles"
 
 for file in ${phpFiles}; do
-	./vendor/bin/phpmd ${file} ${args}
+  ./vendor/bin/phpmd ${file} ${args}
 done
