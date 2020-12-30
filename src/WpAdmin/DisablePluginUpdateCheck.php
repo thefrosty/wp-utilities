bfb2d681377386d32c08e02f55ce6234372e63d2
Austin Passy <367897+thefrosty@users.noreply.github.com>
2020-12-30 15:14:44 -0800

    Add checks for WordPress' API for plugin info calls and bypass removing the plugin from update checks if it uses github-updater.

--- src/WpAdmin/DisablePluginUpdateCheck.php
+++ src/WpAdmin/DisablePluginUpdateCheck.php
@@ -3,6 +3,8 @@
 namespace TheFrosty\WpUtilities\WpAdmin;
 
 use TheFrosty\WpUtilities\Plugin\AbstractHookProvider;
+use TheFrosty\WpUtilities\Plugin\Plugin;
+use WP_Error;
 
 /**
  * Class disablePluginUpdateCheck
@@ -10,7 +12,10 @@ use TheFrosty\WpUtilities\Plugin\AbstractHookProvider;
  */
 class DisablePluginUpdateCheck extends AbstractHookProvider
 {
+
+    private const BYPASS_KEY = 'bypass-http-request';
     private const WP_ORG_UPDATE_CHECK = 'https://api.wordpress.org/plugins/update-check/';
+    private const WP_ORG_PLUGINS_INFO = 'https://api.wordpress.org/plugins/info/';
 
     /**
      * Add class hooks
@@ -18,6 +23,7 @@ class DisablePluginUpdateCheck extends AbstractHookProvider
     public function addHooks(): void
     {
         $this->addFilter('http_request_args', [$this, 'httpRequestRemovePluginBasename'], 10, 2);
+        $this->addFilter('pre_http_request', [$this, 'bypassHttpRequest'], 10, 3);
         $this->addFilter('site_transient_update_plugins', [$this, 'transientRemovePluginBasename']);
     }
 
@@ -37,11 +43,52 @@ class DisablePluginUpdateCheck extends AbstractHookProvider
                 $args['body']['plugins'] = \wp_json_encode($plugins);
             }
         }
+        if (\strpos($url, self::WP_ORG_PLUGINS_INFO) === 0) {
+            \parse_str(\parse_url($url, \PHP_URL_QUERY), $result);
+            if (!empty($result['request']) &&
+                !empty($result['request']['slug']) &&
+                $result['request']['slug'] === $this->getPlugin()->getSlug() &&
+                \__return_true() === false // Re-enable when we can validate this works.
+            ) {
+                $args[self::BYPASS_KEY] = $this->getPlugin()->getSlug();
+            }
+        }
+
         return $args;
     }
 
     /**
+     * Attempt to bypass the HTTP Request if the bypass key is present.
+     * @todo I was initially investigating certain plugins throwing 404's when they should not be called by the
+     * info API endpoint, but don't see an easy way to validate this, so I will "disable" the key in the
+     * `httpRequestRemovePluginBasename` method.
+     * @param false|array|WP_Error $preempt A preemptive return value of an HTTP request. Default false.
+     * @param array $parsed_args HTTP request arguments.
+     * @param string $url The request URL.
+     * @return mixed
+     */
+    protected function bypassHttpRequest($preempt, array $parsed_args, string $url)
+    {
+        if (\strpos($url, self::WP_ORG_PLUGINS_INFO) === 0 &&
+            !empty($parsed_args[self::BYPASS_KEY]) &&
+            $parsed_args[self::BYPASS_KEY] === $this->getPlugin()->getSlug()
+        ) {
+            return new WP_Error(
+                'bypass_http_request',
+                \sprintf(
+                    \esc_html__('The plugin `%s` has requested to bypass api.wp.org/plugin/info.', 'wp-utilities'),
+                    $this->getPlugin()->getSlug(),
+                    ['status' => \WP_Http::NOT_FOUND]
+                )
+            );
+        }
+
+        return $preempt;
+    }
+
+    /**
      * Remove this plugin from the transient value via the core filter.
+     * Ignoring those looking for updating via GitHub Updater.
      * @link https://gist.github.com/rniswonger/ee1b30e5fd3693bb5f92fbcfabe1654d
      * @param mixed $value
      * @return mixed
@@ -49,9 +96,28 @@ class DisablePluginUpdateCheck extends AbstractHookProvider
     protected function transientRemovePluginBasename($value)
     {
         if (isset($value) && \is_object($value) && (!empty($value->response) && \is_array($value->response))) {
-            unset($value->response[$this->getPlugin()->getBasename()]);
+            if (!$this->hasGitHubUpdater()) {
+                unset($value->response[$this->getPlugin()->getBasename()]);
+            }
         }
 
         return $value;
+    }
+
+    /**
+     * Does the current plugin use GitHub Updater?
+     * @link https://github.com/afragen/github-updater
+     * @return bool
+     */
+    private function hasGitHubUpdater(): bool
+    {
+        $key = \sprintf('%s/get_file_data_%s', Plugin::TAG, \sanitize_key($this->getPlugin()->getSlug()));
+        $data = \wp_cache_get($key, 'wp-utilities');
+        if ($data === false) {
+            $data = \get_file_data($this->getPlugin()->getFile(), ['GitHubPluginURI' => 'GitHub Plugin URI'], 'plugin');
+            \wp_cache_set($key, $data, 'wp-utilities', \DAY_IN_SECONDS);
+        }
+
+        return \is_array($data) && !empty($data['GitHubPluginURI']);
     }
 }
