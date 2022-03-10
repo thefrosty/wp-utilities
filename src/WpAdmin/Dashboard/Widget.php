4f0a65dea83adab4f7831bf2757f72df22a838cc
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-03-10 13:44:22 -0800

    Add new data to the Dashboard Widget model to allow for type set (RSS or REST).

--- src/WpAdmin/Dashboard/Widget.php
+++ src/WpAdmin/Dashboard/Widget.php
@@ -3,6 +3,7 @@
 namespace TheFrosty\WpUtilities\WpAdmin\Dashboard;
 
 use TheFrosty\WpUtilities\Models\BaseModel;
+use function in_array;
 
 /**
  * Class Widget
@@ -12,20 +13,52 @@ use TheFrosty\WpUtilities\Models\BaseModel;
 class Widget extends BaseModel
 {
 
+    public const CACHE = 'cache';
+    public const FEED_URL = 'feed_url';
+    public const TYPE = 'type';
+    public const TYPE_REST = 'rest';
+    public const TYPE_RSS = 'rss';
+    public const WIDGET_ID = 'widget_id';
+    public const WIDGET_NAME = 'widget_name';
+
+    /** @var string|null $cache */
+    private ?string $cache;
+
     /** @var string $feed_url */
-    private $feed_url;
+    private string $feed_url;
+
+    /** @var string $type */
+    private string $type = self::TYPE_REST;
 
     /** @var string $widget_id */
-    private $widget_id;
+    private string $widget_id;
 
     /** @var string $widget_name */
-    private $widget_name;
+    private string $widget_name;
+
+    /**
+     * Set the cache type.
+     * @param string|null $cache
+     */
+    protected function setCache(?string $cache = null): void
+    {
+        $this->cache = $cache;
+    }
+
+    /**
+     * Get the cache type.
+     * @return string
+     */
+    public function getCache(): ?string
+    {
+        return $this->cache;
+    }
 
     /**
      * Set the feed URL.
      * @param string $url
      */
-    protected function setFeedUrl(string $url)
+    protected function setFeedUrl(string $url): void
     {
         $this->feed_url = $url;
     }
@@ -43,9 +76,30 @@ class Widget extends BaseModel
      * Set the widget ID.
      * @param string $widget_id
      */
-    protected function setWidgetId(string $widget_id)
+    protected function setWidgetId(string $widget_id): void
     {
         $this->widget_id = $widget_id;
+    }
+
+    /**
+     * Get the request type.
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the request type.
+     * @param string $type
+     */
+    protected function setType(string $type): void
+    {
+        if (!in_array(\strtolower($type), [self::TYPE_REST, self::TYPE_RSS], true)) {
+            return;
+        }
+        $this->type = $type;
     }
 
     /**
@@ -61,7 +115,7 @@ class Widget extends BaseModel
      * Set the widget name.
      * @param string $widget_name
      */
-    protected function setWidgetName(string $widget_name)
+    protected function setWidgetName(string $widget_name): void
     {
         $this->widget_name = $widget_name;
     }
