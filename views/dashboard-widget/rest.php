59bac2ed6b9115f79aa37b314b9e804ff07b40e8
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-03-14 17:20:03 -0700

    Allow view type overloading view via variable null coalescing assignment operator.

--- views/dashboard-widget/rest.php
+++ views/dashboard-widget/rest.php
@@ -6,6 +6,8 @@ $wpRemote = new class {
 };
 /** @var $this TheFrosty\WpUtilities\WpAdmin\DashboardWidget */
 $posts ??= $wpRemote->retrieveBodyCached($this->getWidget()->getFeedUrl(), DAY_IN_SECONDS);
+$renderContent ??= true; // Pass false to disable rendering the widget content on the first key.
+$widgetId ??= $this->getWidget()->getWidgetId(); // Pass the widget ID to the template (outside `DashboardWidget`).
 static $count;
 
 $content = '';
@@ -19,10 +21,10 @@ if (empty($posts)) {
         $content .= '<a class="rsswidget" href="' . esc_url(add_query_arg([
                 'utm_medium' => 'wpadmin_dashboard',
                 'utm_term' => 'newsitem',
-                'utm_campaign' => $this->getWidget()->getWidgetId(),
+                'utm_campaign' => $widgetId,
             ], $item->link)) . '">' . esc_html($item->title->rendered) . '</a>';
 
-        if ($count === 1) {
+        if ($count === 1 && $renderContent) {
             $content .= '&nbsp;&nbsp;&nbsp;<span class="rss-date">' .
                 date_i18n(get_option('date_format'), strtotime($item->date)) . '</span>';
             $content .= '<div class="rssSummary">' . strip_tags(wp_trim_words($item->content->rendered, 28)) . '</div>';
