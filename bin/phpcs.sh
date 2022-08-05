6322940f240d71164f1d20a4183f65316120ca32
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-08-05 12:27:54 -0700

    Move shared code into functions.sh. and pass argument flags to args.

--- bin/phpcs.sh
+++ bin/phpcs.sh
@@ -5,21 +5,9 @@ set -e
 # Based off: https://gist.github.com/Hounddog/3891872
 # Go to root of the repository
 echo 'Checking PHPCS'
+source functions.sh
 
-if [[ $(git rev-parse --verify HEAD) ]]; then
-  against='HEAD'
-elif [[ $(git rev-parse --verify develop) ]]; then
-  against='develop'
-elif [[ $(git rev-parse --verify master) ]]; then
-  against='master'
-else
-  echo "git can't verify HEAD, develop or master."
-  exit 1
-fi
-
-commitFiles=$(git diff --name-only "$(git merge-base develop ${against})")
-
-args="-s --colors --extensions=php --tab-width=4 --standard=phpcs-ruleset.xml --report=xml --runtime-set testVersion 7.4-"
+args="-s --colors --extensions=php --tab-width=4 --standard=phpcs-ruleset.xml --report=xml --runtime-set testVersion ${TEST_VERSION}-"
 phpFiles=""
 phpFilesCount=0
 for f in ${commitFiles}; do
@@ -38,7 +26,7 @@ fi
 
 echo "Checking files: $phpFiles"
 
-if [[ ${phpFilesCount} -gt 2 ]] && { [[ ${TRAVIS+x} ]] || [[ ${CIRCLECI+x} ]]; }; then
+if [[ ${phpFilesCount} -gt 2 ]] && { [[ ${GITHUB_ACTIONS+x} ]] || [[ ${CIRCLECI+x} ]]; }; then
   args="$args --report=summary"
 fi
 
