e77b3c204d82b7fe2e283aed71bf6cacd96ac7ba
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-08-05 13:31:36 -0700

    Add fallback for default.

--- bin/functions.sh
+++ bin/functions.sh
@@ -4,28 +4,27 @@ set -e
 
 # Default values of arguments
 # https://stackoverflow.com/a/44750379/558561 -- get the default git branch name
-DEFAULT_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@') || echo "develop"
+DEFAULT_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
 TEST_VERSION="7.4"
 OTHER_ARGUMENTS=()
 
 # Loop through arguments and process them
 # @ref https://pretzelhands.com/posts/command-line-flags/
-for arg in "$@"
-do
-    case $arg in
-        --default-branch)
-        DEFAULT_BRANCH="$2"
-        shift 2 # Remove name and value
-        ;;
-        --test-version)
-        TEST_VERSION="$2"
-        shift 2 # Remove name and value
-        ;;
-        *)
-        OTHER_ARGUMENTS+=("$1")
-        shift # Remove generic argument from processing
-        ;;
-    esac
+for arg in "$@"; do
+  case $arg in
+  --default-branch)
+    DEFAULT_BRANCH="$2"
+    shift 2 # Remove name and value
+    ;;
+  --test-version)
+    TEST_VERSION="$2"
+    shift 2 # Remove name and value
+    ;;
+  *)
+    OTHER_ARGUMENTS+=("$1")
+    shift # Remove generic argument from processing
+    ;;
+  esac
 done
 
 # Based off: https://gist.github.com/Hounddog/3891872
@@ -42,4 +41,4 @@ else
   exit 1
 fi
 
-commitFiles=$(git diff --name-only "$(git merge-base ${DEFAULT_BRANCH} ${against})")
+commitFiles=$(git diff --name-only "$(git merge-base ${DEFAULT_BRANCH:-develop} ${against})")
