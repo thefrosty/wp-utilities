534c13414d3df12d6e249b9f322aed47ff903613
Austin Passy <367897+thefrosty@users.noreply.github.com>
2023-05-11 09:19:54 -0700

    Code format cleanup.

--- src/Api/WpCacheTrait.php
+++ src/Api/WpCacheTrait.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace TheFrosty\WpUtilities\Api;
 
@@ -8,7 +10,6 @@ use function wp_cache_set;
 
 /**
  * Trait WpCacheTrait
- *
  * @package TheFrosty\WpUtilities\Api
  */
 trait WpCacheTrait
@@ -59,7 +60,6 @@ trait WpCacheTrait
 
     /**
      * Optional. Set the cache group the current query.
-     *
      * @param string $group
      */
     public function setCacheGroup(string $group): void
@@ -69,14 +69,12 @@ trait WpCacheTrait
 
     /**
      * Retrieve object from cache.
-     *
      * @param string $key The key under which to store the value.
      * @param string|null $group The group value appended to the $key.
-     * @param bool $force Optional. Whether to force an update of the local cache from the persistent
-     *                                cache. Default false.
-     * @param bool $found Optional. Whether the key was found in the cache. Disambiguate a return of false,
-     *                                a storable value. Passed by reference. Default null.
-     *
+     * @param bool $force Optional. Whether to force an update of the local cache from the persistent cache. Default
+     *     false.
+     * @param bool $found Optional. Whether the key was found in the cache. Disambiguate a return of false, a storable
+     *     value. Passed by reference. Default null.
      * @return mixed Cached object value.
      * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
      */
@@ -87,25 +85,21 @@ trait WpCacheTrait
 
     /**
      * Sets a value in cache.
-     *
      * @param string $key The key under which to store the value.
      * @param mixed $value The value to store.
      * @param string|null $group The group value appended to the $key.
      * @param int $expiration The expiration time, defaults to 0.
-     *
      * @return bool Returns TRUE on success or FALSE on failure.
      */
-    public function setCache(string $key, $value, ?string $group = null, int $expiration = 0): bool
+    public function setCache(string $key, mixed $value, ?string $group = null, int $expiration = 0): bool
     {
         return wp_cache_set($key, $value, $group ?? $this->getCacheGroup(), $expiration);
     }
 
     /**
      * Deletes a value in cache.
-     *
      * @param string $key The key under which the value is stored.
      * @param string|null $group The group value appended to the $key.
-     *
      * @return bool Returns TRUE on success or FALSE on failure.
      */
     public function deleteCache(string $key, ?string $group = null): bool
