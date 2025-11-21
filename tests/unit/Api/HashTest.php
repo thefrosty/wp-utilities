bc89f0eeaec52ad6ff620039202e51f9722af900
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-11-21 09:35:53 -0800

    Add additional tests

--- tests/unit/Api/HashTest.php
+++ tests/unit/Api/HashTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace TheFrosty\WpUtilities\Tests\Api;
 
 use PHPUnit\Framework\Attributes\CoversClass;
+use RuntimeException;
 use TheFrosty\WpUtilities\Api\Hash;
 use TheFrosty\WpUtilities\Tests\Plugin\Framework\TestCase;
 
@@ -56,11 +57,35 @@ class HashTest extends TestCase
         $this->assertEquals('test data', $decrypted_data);
     }
 
-    public function testDecrypt(): void
+    public function testEncryptSuccess(): void
     {
+        $original_data = 'test data';
+        $encryption_key = 'SomeKeyWith4Delimiter|';
+
+        // Encrypt the data and assert that it is not equal to the original data
+        $encrypted_data = $this->hash->encrypt($original_data, $encryption_key);
+        $this->assertNotEquals($original_data, $encrypted_data);
+
+        // Decrypt the encrypted data and assert that it matches the original data
+        $decrypted_data = $this->hash->decrypt($encrypted_data, $encryption_key);
+        $this->assertEquals($original_data, $decrypted_data);
     }
 
-    public function testEncrypt(): void
+    public function testEncryptErrorHandling(): void
     {
+
+        $data = 'test data';
+        $encryption_key = 'SomeKeyWith4Delimiter|';
+
+        // Override the encrypt method to simulate encryption failure
+        $originalEncryptMethod = $this->reflection->getMethod('encrypt');
+        $this->setReflectionMethod($originalEncryptMethod, static function () {
+            throw new RuntimeException('Encryption failed');
+        });
+
+        // Call the encrypt method and expect an exception
+        $encrypted_data = $this->hash->encrypt($data, $encryption_key);
+        // Mock openssl_encrypt to throw an exception
+        $this->expectException(RuntimeException::class);
     }
 }
