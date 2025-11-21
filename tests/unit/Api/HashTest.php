22e313416e916646b77854623d4c9a7b08aaba1b
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-11-21 11:09:30 -0800

    Update unit tests

--- tests/unit/Api/HashTest.php
+++ tests/unit/Api/HashTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace TheFrosty\WpUtilities\Tests\Api;
 
 use PHPUnit\Framework\Attributes\CoversClass;
-use RuntimeException;
 use TheFrosty\WpUtilities\Api\Hash;
 use TheFrosty\WpUtilities\Tests\Plugin\Framework\TestCase;
 
@@ -29,74 +28,45 @@ class HashTest extends TestCase
     public function testGetHashedKey(): void
     {
         $data = 'example_input';
-        $expected = hash('sha256', $data, true);
+        $expected = hash('sha256', $data);
         $actual = $this->reflection->getMethod('getHashedKey')->invoke($this->hash, $data);
+        $this->assertTrue(method_exists($this->hash, 'getHashedKey'));
         $this->assertEquals($expected, $actual);
     }
 
-    public function testDecryptWithDelimiter(): void
-    {
-        $original_data = 'test data';
-        $encryption_key = 'SomeKeyWith4Delimiter|';
-
-        // Encrypt the data first to get a valid encrypted string with delimiter
-        $encrypted_data = $this->reflection->getMethod('encrypt')->invoke($this->hash, $original_data, $encryption_key);
-
-        // Decrypt the data and assert that it matches the original data
-        $decrypted_data = $this->reflection->getMethod('decrypt')->invoke(
-            $this->hash,
-            $encrypted_data,
-            $encryption_key
-        );
-        $this->assertEquals($original_data, $decrypted_data);
-    }
-
-    public function testDecryptWithoutDelimiter(): void
-    {
-        $base64_encoded_data = base64_encode('test data');
-        $encryption_key = 'SomeKeyWith4Delimiter|';
-
-        // Decrypt the data without delimiter and assert that it matches the original data
-        $decrypted_data = $this->reflection->getMethod('decrypt')->invoke(
-            $this->hash,
-            $base64_encoded_data,
-            $encryption_key
-        );
-        $this->assertEquals('test data', $decrypted_data);
-    }
-
-    public function testEncryptSuccess(): void
-    {
-        $original_data = 'test data';
-        $encryption_key = 'SomeKeyWith4Delimiter|';
-
-        // Encrypt the data and assert that it is not equal to the original data
-        $encrypted_data = $this->reflection->getMethod('encrypt')->invoke($this->hash, $original_data, $encryption_key);
-        $this->assertNotEquals($original_data, $encrypted_data);
-
-        // Decrypt the encrypted data and assert that it matches the original data
-        $decrypted_data = $this->reflection->getMethod('decrypt')->invoke(
-            $this->hash,
-            $encrypted_data,
-            $encryption_key
-        );
-        $this->assertEquals($original_data, $decrypted_data);
-    }
-
-    public function testEncryptErrorHandling(): void
+    public function testDecrypt(): void
     {
         $data = 'test data';
-        $encryption_key = 'SomeKeyWith4Delimiter|';
 
-        // Override the encrypt method to simulate encryption failure
-        $originalEncryptMethod = $this->reflection->getMethod('encrypt');
-        $this->setReflectionMethod($originalEncryptMethod, static function () {
-            throw new RuntimeException('Encryption failed');
-        });
+        // Encrypt the data first to get a valid encrypted string with delimiter
+        $encrypted_data = $this->encrypt($data);
+        $this->assertNotEquals($data, $encrypted_data);
 
-        // Call the encrypt method and expect an exception
-        $encrypted_data = $this->reflection->getMethod('encrypt')->invoke($this->hash, $data, $encryption_key);
-        // Mock openssl_encrypt to throw an exception
-        $this->expectException(RuntimeException::class);
+        // Decrypt the data and assert that it matches the original data
+        $decrypted_data = $this->decrypt($encrypted_data);
+        $this->assertEquals($data, $decrypted_data);
+    }
+
+    public function testEncrypt(): void
+    {
+        $data = 'test data';
+
+        // Encrypt the data and assert that it is not equal to the original data
+        $encrypted_data = $this->encrypt($data);
+        $this->assertNotEquals($data, $encrypted_data);
+
+        // Decrypt the encrypted data and assert that it matches the original data
+        $decrypted_data = $this->decrypt($encrypted_data);
+        $this->assertEquals($data, $decrypted_data);
+    }
+
+    private function decrypt(string $data): string
+    {
+        return $this->reflection->getMethod('decrypt')->invoke($this->hash, $data);
+    }
+
+    private function encrypt(string $data): string
+    {
+        return $this->reflection->getMethod('encrypt')->invoke($this->hash, $data);
     }
 }
