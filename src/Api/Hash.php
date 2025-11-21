3b37c70a7a2dff6f9cb4927b7ae3c20838bf0a05
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-11-21 11:09:23 -0800

    Update Hash encryption to use Illuminate\Encryption\Encrypter

--- src/Api/Hash.php
+++ src/Api/Hash.php
@@ -4,18 +4,7 @@ declare(strict_types=1);
 
 namespace TheFrosty\WpUtilities\Api;
 
-use Random\RandomException;
-use RuntimeException;
-use function base64_decode;
-use function base64_encode;
-use function explode;
-use function hash;
-use function openssl_cipher_iv_length;
-use function openssl_decrypt;
-use function openssl_encrypt;
-use function random_bytes;
-use function str_contains;
-use const OPENSSL_RAW_DATA;
+use Illuminate\Encryption\Encrypter;
 
 /**
  * Trait Hash
@@ -24,7 +13,7 @@ use const OPENSSL_RAW_DATA;
 trait Hash
 {
 
-    private const string CIPHER = 'AES-256-CBC';
+    private const string CIPHER = 'aes-256-cbc';
 
     /**
      * Get a sha256 hash key.
@@ -33,49 +22,36 @@ trait Hash
      */
     protected function getHashedKey(string $data): string
     {
-        return hash('sha256', $data, true);
+        return hash('sha256', $data);
     }
 
     /**
      * Decrypt a string.
      * @param string $data The encrypted string value.
-     * @param string $encryption_key The encryption key.
      * @return string
      */
-    protected function decrypt(string $data, string $encryption_key): string
+    protected function decrypt(string $data): string
     {
-        $key = $this->getHashedKey($encryption_key);
-        if (!str_contains($data, '::')) {
-            return base64_decode($data);
-        }
-        [$encrypted_data_base64, $iv_base64] = explode('::', $data, 2);
-        $encrypted_data = base64_decode($encrypted_data_base64);
-        $iv = base64_decode($iv_base64);
-
-        return openssl_decrypt($encrypted_data, self::CIPHER, $key, OPENSSL_RAW_DATA, $iv);
+        return $this->getEncrypter($data)->decryptString($data);
     }
 
     /**
      * Encrypt a string.
      * @param string $data The string value to encrypt
-     * @param string $encryption_key The encryption key. Example `SomeKeyWith4Delimiter|`.
      * @return string
-     * @throws RuntimeException
      */
-    protected function encrypt(string $data, string $encryption_key): string
+    protected function encrypt(string $data): string
     {
-        $key = $this->getHashedKey($encryption_key);
-        // Is the encryption method is cryptographically strong?
-        $iv_length = openssl_cipher_iv_length(self::CIPHER);
-        try {
-            $iv = random_bytes($iv_length);
-            $encrypted_data = openssl_encrypt($data, self::CIPHER, $key, OPENSSL_RAW_DATA, $iv);
-            if ($encrypted_data === false) {
-                throw new RuntimeException('Encryption failed');
-            }
-            return base64_encode($encrypted_data . '::' . base64_encode($iv)); // Append IV to encrypted data.
-        } catch (RandomException) {
-            return base64_encode($data); // Return unencrypted on error as base64 encode.
-        }
+        return $this->getEncrypter($data)->encryptString($data);
+    }
+
+    /**
+     * Create an instance of Encrypter.
+     * @param string $key
+     * @return Encrypter
+     */
+    private function getEncrypter(string $key): Encrypter
+    {
+        return new Encrypter($key, self::CIPHER);
     }
 }
