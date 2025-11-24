031567103f558778652ce67d5bdc00400d84f4b3
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-11-24 14:32:27 -0800

    Enhancements to encryption with passed keys as optional

--- src/Api/Hash.php
+++ src/Api/Hash.php
@@ -5,18 +5,24 @@ declare(strict_types=1);
 namespace TheFrosty\WpUtilities\Api;
 
 use Illuminate\Encryption\Encrypter;
+use RuntimeException;
+use SensitiveParameter;
 use function add_site_option;
 use function apply_filters;
 use function base64_decode;
 use function base64_encode;
 use function class_exists;
+use function explode;
 use function get_site_option;
 use function hash;
+use function openssl_cipher_iv_length;
 use function openssl_decrypt;
 use function openssl_encrypt;
+use function random_bytes;
 use function sprintf;
 use function substr;
 use function wp_generate_password;
+use const OPENSSL_RAW_DATA;
 use const TheFrosty\WpUtilities\CIPHER;
 use const TheFrosty\WpUtilities\ENCRYPTION_KEY_OPTION;
 
@@ -31,39 +37,65 @@ trait Hash
     /**
      * Decrypt a string.
      * @param string $data The encrypted string value.
-     * @param string $encryption_key
+     * @param string|null $encryption_key
      * @return string
+     * @throws RuntimeException
      */
-    public function decrypt(string $data, string $encryption_key): string
+    public function decrypt(string $data, ?string $encryption_key = null): string
     {
-        $encryptor = self::getEncrypter();
+        $encryptor = self::getEncrypter($encryption_key);
         if ($encryptor && self::useEncrypter()) {
             return $encryptor->decryptString($data);
         }
 
-        $key = $this->getHashedKey($encryption_key);
-        $vector = substr($this->getHashedKey(sprintf('%s_iv', $encryption_key)), 0, 16);
+        if ($encryption_key === null) {
+            $parts = explode('::', $data);
+            if (empty($parts[0]) || empty($parts[1])) {
+                throw new RuntimeException('Could not decrypt the data.');
+            }
+            $iv = base64_decode($parts[0]);
+            $ciphertext = base64_decode($parts[1]);
+            if ($iv === false || $ciphertext === false) {
+                throw new RuntimeException('The data is invalid.');
+            }
 
-        return openssl_decrypt(base64_decode($data), 'AES-256-CBC', $key, 0, $vector);
-    }
-
-    /**
-     * Encrypt a string.
-     * @param string $data The string value to encrypt
-     * @param string $encryption_key
-     * @return string
-     */
-    public function encrypt(string $data, string $encryption_key): string
-    {
-        $encryptor = self::getEncrypter();
-        if ($encryptor && self::useEncrypter()) {
-            return $encryptor->encryptString($data);
+            return openssl_decrypt($ciphertext, CIPHER, self::getEncryptionKey(), OPENSSL_RAW_DATA, $iv);
         }
 
         $key = $this->getHashedKey($encryption_key);
         $vector = substr($this->getHashedKey(sprintf('%s_iv', $encryption_key)), 0, 16);
 
-        return base64_encode(openssl_encrypt($data, 'AES-256-CBC', $key, 0, $vector));
+        return openssl_decrypt(base64_decode($data), CIPHER, $key, 0, $vector);
+    }
+
+    /**
+     * Encrypt a string.
+     * @param mixed $data The string value to encrypt
+     * @param string|null $encryption_key
+     * @return string
+     * @throws \Random\RandomException
+     */
+    public function encrypt(#[SensitiveParameter] mixed $data, ?string $encryption_key = null): string
+    {
+        $encryptor = self::getEncrypter($encryption_key);
+        if ($encryptor && self::useEncrypter()) {
+            return $encryptor->encryptString($data);
+        }
+
+        if ($encryption_key === null) {
+            $iv = random_bytes(openssl_cipher_iv_length(CIPHER));
+
+            return sprintf(
+                '%1$s":%2$s',
+                base64_encode($iv),
+                base64_encode(openssl_encrypt($data, CIPHER, self::getEncryptionKey(), OPENSSL_RAW_DATA, $iv))
+            );
+        }
+
+        $key = $this->getHashedKey($encryption_key);
+        $vector = substr($this->getHashedKey(sprintf('%s_iv', $encryption_key)), 0, 16);
+
+        return base64_encode(openssl_encrypt($data, CIPHER, $key, 0, $vector));
     }
 
     /**
@@ -84,22 +116,24 @@ trait Hash
 
     /**
      * Get a sha256 hash key.
-     * @param string $data
+     * @param string|null $data
      * @return string
      */
-    protected function getHashedKey(string $data): string
+    protected function getHashedKey(?string $data = null): string
     {
+        $data ??= static::class;
         return hash('sha256', $data);
     }
 
     /**
      * Create an instance of Encrypter.
+     * @param string|null $key
      * @return Encrypter|null
      */
-    private static function getEncrypter(): ?Encrypter
+    private static function getEncrypter(?string $key = null): ?Encrypter
     {
         static $encrypter;
-        $key = self::getEncryptionKey();
+        $key ??= self::getEncryptionKey();
         if (class_exists(Encryter::class)) {
             $encrypter ??= new Encrypter($key, CIPHER);
         }
@@ -112,6 +146,6 @@ trait Hash
      */
     protected static function useEncrypter(): bool
     {
-        return apply_filters('wp_utilities_use_encrypter_package', false, self::class) === true;
+        return apply_filters('wp_utilities_use_encrypter_package', false, static::class) === true;
     }
 }
